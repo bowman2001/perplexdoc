0a946875ed3eda9dc6e2a6130383840b5babb371
Georg Makowski <gm@desider.at>
2022-04-12 11:33:19 +0200

    Better overlay
    Signed-off-by: Georg Makowski <gm@desider.at>

--- themes/perplex/assets/js/toggles.js
+++ themes/perplex/assets/js/toggles.js
@@ -11,61 +11,58 @@ function handleToggle(e) {
 toggleInterface.addEventListener('keydown', handleToggle); */
 
 
-let sbBtn1 = document.querySelector('#tgl-sb1');
-let sbBtn2 = document.querySelector('#tgl-sb2');
-let sb = document.querySelector('#sidebar');
-let main = document.querySelector('#main');
+const sbBtn1 = document.querySelector('#tgl-sb1');
+const sbBtn2 = document.querySelector('#tgl-sb2');
+const sb = document.querySelector('#sidebar');
+const overlay = document.querySelector('#overlay');
 
 
-let tocBtn = document.querySelector('#toggle-toc');
-let toc = document.querySelector('#toc');
-
+const tocBtn = document.querySelector('#toggle-toc');
+const toc = document.querySelector('#toc');
 
 if( sbBtn1 ) {
     sbBtn1.addEventListener('click', function () {
-        if ( sb ) {
-            sbBtn1.classList.toggle('is-visible');
-            sbBtn2.classList.toggle('is-visible');
-            sb.classList.toggle('is-visible');
-            main.classList.toggle('is-locked');
+           sbHandler();
         }
-        if ( sb.classList.contains( 'is-visible' ) ) {
-            sbBtn1.title = "Close the sidebar navigation.";
-            sbBtn2.title = "Close the sidebar navigation.";
-        } else {
-            sbBtn2.title = "Open the sidebar navigation.";
-        }
-        if( tocBtn ) {
-            tocBtn.disabled = !tocBtn.disabled;
-        }
-    })
+    );
 }
 
 if( sbBtn2 ) {
     sbBtn2.addEventListener('click', function () {
-        if ( sb ) {
-            sbBtn1.classList.toggle('is-visible');
-            sbBtn2.classList.toggle('is-visible');
-            sb.classList.toggle('is-visible');
-            main.classList.toggle('is-locked');
+            sbHandler();
         }
-        if ( sb.classList.contains( 'is-visible' ) ) {
-            sbBtn1.title = "Close the sidebar navigation.";
-            sbBtn2.title = "Close the sidebar navigation.";
-        } else {
-            sbBtn1.title = "Open the sidebar navigation.";
-            sbBtn2.title = "Open the sidebar navigation.";
-        }
-        if( tocBtn ) {
-            tocBtn.disabled = !tocBtn.disabled;
-        }
-    })
+    );
 }
 
+if( overlay ) {
+    overlay.addEventListener('click', function () {
+            sbHandler();
+        }
+    );
+}
 
 if ( tocBtn ) {
     tocBtn.addEventListener('click', function () {
         tocBtn.classList.toggle( 'is-visible' );
         toc.classList.toggle('is-closed');
     })
+}
+
+function sbHandler () {
+    if ( sb ) {
+        sbBtn1.classList.toggle('is-visible');
+        sbBtn2.classList.toggle('is-visible');
+        sb.classList.toggle('is-visible');
+        overlay.classList.toggle('is-visible');
+    }
+    if ( sb.classList.contains( 'is-visible' ) ) {
+        sbBtn1.title = "Close the sidebar navigation.";
+        sbBtn2.title = "Close the sidebar navigation.";
+    } else {
+        sbBtn1.title = "Open the sidebar navigation.";
+        sbBtn2.title = "Open the sidebar navigation.";
+    }
+    if( tocBtn ) {
+        tocBtn.disabled = !tocBtn.disabled;
+    }
 }
